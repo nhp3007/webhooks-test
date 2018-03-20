8f2cdd171472c440381740dc117c96303fc30fef
Morgan Pittkin <mpittkin@performair.com>
2018-03-20 12:54:14 -0700

    Adds testCapitalizeAlreadyCapitalized

--- src/test/java/com/performair/jenkins/test/StringCapitalizerTest.java
+++ src/test/java/com/performair/jenkins/test/StringCapitalizerTest.java
@@ -15,4 +15,11 @@ public class StringCapitalizerTest
 		assertEquals("String should be capitalized", StringCapitalizer.capitalize(s), "MORGAN");
 	}
 
+	@Test
+	public void testCapitalizeAlreadyCapitalized()
+	{
+		String s = "MORGAN";
+		assertEquals("String should be capitalized", StringCapitalizer.capitalize(s), "MORGAN");
+	}
+
 }
